e9cefee0350a5bfc6d6be1e12928c3b4f4aabe8d
rainwang007 <rainwangjiantao@gmail.com>
2023-06-02 16:15:00 +0800

    add About props

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function About() {
-    return (
-      <div>
-        <h1>About Me</h1>
-        <p>
-          I'm a full stack web developer with a passion for creating intuitive and
-          engaging digital experiences. I specialize in React, Redux, Node.js, and
-          other modern web technologies. 
-        </p>
-      </div>
-    );
-  }
-  
-  export default About;
-  
+function About(props) {
+  const [showMore, setShowMore] = useState(false);
+
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        I'm {props.name}, a {props.jobTitle} with a passion for creating intuitive and
+        engaging digital experiences. I specialize in {props.specialization}.
+      </p>
+      {showMore &&
+        <div>
+          <h2>More About Me</h2>
+          <p>
+            I live in {props.location} and I've been a developer for {props.yearsOfExperience} years.
+          </p>
+        </div>
+      }
+      <button onClick={() => setShowMore(!showMore)}>
+        {showMore ? "Show Less" : "Show More"}
+      </button>
+    </div>
+  );
+}
+
+export default About;
